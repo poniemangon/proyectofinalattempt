f51c4c52bcbdcb09f869e1f5d8b476c8bb8b4ebc
gon_14 <poniemangon@gmail.com>
2022-07-20 22:05:15 -0300

    un par de cosas piola

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomeView, ArticleDetailView
+from .views import HomeView, ArticleDetailView, NewArticleView
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
-    path('article/<int:pk>', ArticleDetailView.as_view(), name="article-detail")
+    path('article/<int:pk>', ArticleDetailView.as_view(), name="article-detail"),
+    path('new_post', NewArticleView.as_view(), name='new_post')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from blog.models import Post
 
 # Create your views here.
@@ -14,3 +14,7 @@ class ArticleDetailView(DetailView):
     model = Post
     template_name = 'blog/article_details.html'
      
+class NewArticleView(CreateView):
+    model = Post
+    template_name = 'blog/new_post.html'
+    fields = '__all__'
